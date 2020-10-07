18744f9500d2a2958e09a49175bf02662ba5842e
Tom Overland <tomoverland03@gmail.com>
2020-10-06 21:54:05 -0500

    fixed err with connection var

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ const secrets = require("./config");
 const util = require("util");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: secrets.username,
