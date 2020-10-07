2d350af50fe76093029135a2aacccb96b1ab04f7
Tom Overland <tomoverland03@gmail.com>
2020-10-07 18:01:35 -0500

    fixed burger_controller to have the same methods as orm.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-  burgers.all(function (data) {
+  burgers.selectAll(function (data) {
     var hbsObject = {
       burgers: data,
     };
@@ -14,33 +14,25 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers", function (req, res) {
-  burgers.create(
-    ["name", "devoured"],
-    [req.body.name, req.body.devoured],
-    function (result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
+router.post("/burgers", function (req, res) {
+  burgers.insertOne(["burger_name"], [req.body.burger_name], function () {
+    // Send back the ID of the new quote
+    res.redirect("/");
+  });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+  // console.log("condition", condition);
 
-  burgers.update(
+  burgers.updateOne(
     {
-      devoured: req.body.devoured,
+      devoured: true,
     },
     condition,
-    function (result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+    function (data) {
+      res.redirect("/");
     }
   );
 });
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE burgers_db;
 USE burgers_DB;
 
-CREATE TABLE burgers(
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(30) NOT NULL,
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+    burger_name varchar
+    (30) NOT NULL,
     devoured boolean DEFAULT false,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 )
