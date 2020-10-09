ed9ad4ca6dcdec76cb59acc617b2a76bcf8cd37f
Tom Overland <tomoverland03@gmail.com>
2020-10-08 20:15:05 -0500

    added delete function to the burgers.js model

--- models/burgers.js
+++ models/burgers.js
@@ -1,19 +1,27 @@
 var orm = require("../config/orm");
 
 var burger = {
+  // Display all burgers
   selectAll: function (cb) {
     orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+  // Add new burger
   insertOne: function (cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
+  // Change burger's devoured status to true
   updateOne: function (objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  // Delete burger from the db
+  deleteOne: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
       cb(res);
     });
   },
