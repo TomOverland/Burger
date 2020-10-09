fa0472579c01f2c7ee22ef6716e1c86427538e66
Tom Overland <tomoverland03@gmail.com>
2020-10-08 20:12:33 -0500

    added delete function to burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,7 @@ const burger = require("../models/burgers.js");
 var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
+// Get the burgers DB and display it
 router.get("/", function (req, res) {
   burger.selectAll(function (data) {
     var hbsObject = {
@@ -14,14 +15,16 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/burgers", function (req, res) {
+// Add new burger to the db
+router.post("/api/burgers", function (req, res) {
   burger.insertOne(["burger_name"], [req.body.burger_name], function () {
     // Send back the ID of the new quote
     res.redirect("/");
   });
 });
 
-router.put("/burgers/:id", function (req, res) {
+// Changes devoured status to true
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   // console.log("condition", condition);
@@ -37,6 +40,20 @@ router.put("/burgers/:id", function (req, res) {
   );
 });
 
+// Delete burger from the db
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  // console.log("condition", condition)
+
+  burger.deleteOne(condition, function (result) {
+    if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404.
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
